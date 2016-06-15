f1f95022d5d4a64f4a7e3785c773aff901705dbb
Camilo Torres <ctorres@tpipr.com>
2016-06-15 10:54:24 -0400

    Adds test example

---
+++ tests/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ClickMeeting\Entity;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that true does in fact equal true.
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
