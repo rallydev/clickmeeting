b4eea82e294f7ea7586445781a05ad6f4f91bc54
Camilo Torres <ctorres@tpipr.com>
2016-06-14 23:49:38 -0400

    Adds storage entity

--- src/Entity/Storage.php
+++ src/Entity/Storage.php
@@ -13,7 +13,114 @@ namespace ClickMeeting\Entity;
  *
  * @author Alexis J. Rosa Rivera <alexisjrosarivera@gmail.com>
  */
-class Storage
+class Storage extends EntityAbstract
 {
-    //put your code here
+	/**
+	 * [getFileLibrary description]
+	 * @return [type] [description]
+	 */
+    public function getFileLibrary()
+    {
+		return $this->client->fileLibrary();
+	}
+
+	/**
+	 * [getConferenceFileLibrary description]
+	 * @param  Int    $room_id [description]
+	 * @return [type]          [description]
+	 */
+	public function getConferenceFileLibrary()
+	{
+		return $this->client->conferenceFileLibrary($this->$room_id);
+	}
+
+	/**
+	 * [getfileLibraryFile description]
+	 * @param  Int    $file_id [description]
+	 * @return [type]          [description]
+	 */
+    public function getfileLibraryFile(Int $file_id)
+    {
+    	return $this->client->fileLibraryFile($file_id);
+   	}
+
+   	/**
+   	 * [postFileLibraryFile description]
+   	 * @param  String $file_path [description]
+   	 * @return [type]            [description]
+   	 */
+   	public function postFileLibraryFile(String $file_path)
+   	{
+	   	return $this->client->addFileLibraryFile($file_path);
+   	}
+
+   	/**
+   	 * [downloadfileLibraryContent description]
+   	 * @param  Int    $file_id [description]
+   	 * @return [type]          [description]
+   	 */
+   	public function downloadfileLibraryContent(Int $file_id)
+   	{
+   		return $this->client->fileLibraryContent($file_id);
+   	}
+
+   	/**
+   	 * [removeFileLibraryFile description]
+   	 * @param  Int    $file_id [description]
+   	 * @return [type]          [description]
+   	 */
+   	public function removeFileLibraryFile(Int $file_id)
+   	{
+   		return $this->client->deleteFileLibraryFile($file_id);
+   	}
+
+   	/**
+   	 * [getConferenceRecordings description]
+   	 * @param  Int    $room_id [description]
+   	 * @return [type]          [description]
+   	 */
+   	public function getConferenceRecordings()
+   	{
+   		return $this->client->conferenceRecordings($this->$room_id);
+   	}
+
+   	/**
+   	 * [removeConferenceRecordings description]
+   	 * @param  Int    $room_id [description]
+   	 * @return [type]          [description]
+   	 */
+   	public function removeConferenceRecordings()
+   	{
+   		return $this->client->deleteConferenceRecordings($this->$room_id);
+   	}
+
+   	/**
+   	 * [removeConferenceRecording description]
+   	 * @param  Int    $room_id      [description]
+   	 * @param  Int    $recording_id [description]
+   	 * @return [type]               [description]
+   	 */
+   	public function removeConferenceRecording(Int $recording_id)
+   	{
+   		return $this->client->deleteConferenceRecording($this->$room_id, $recording_id);
+   	}
+
+   	/**
+   	 * [getChats description]
+   	 * @return [type] [description]
+   	 */
+   	public function getChats()
+   	{
+   		return $this->client->chats();
+   	}
+
+   	/**
+   	 * [getConferenceSessionChats description]
+   	 * @param  Int    $session_id [description]
+   	 * @return [type]             [description]
+   	 */
+	public function getConferenceSessionChats(Int $session_id)
+	{
+   		return $this->client->conferenceSessionChats($session_id);
+	}
 }
